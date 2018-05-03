41b40cc9eefce468e68b6ac7febb1aac0c822028
Joydeep Mukherjee <joydeep1701@gmail.com>
2018-05-03 22:12:54 +0530

    Boilerplate code for React, Django, Heroku

--- CrowdAlert/settings.py
+++ CrowdAlert/settings.py
@@ -133,3 +133,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
