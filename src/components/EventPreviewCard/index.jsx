e96902294bab7ab35538a6a9068e326139a245c2
Joydeep Mukherjee <joydeep1701@gmail.com>
2018-06-22 21:41:45 +0530

    Close preview on route change

--- src/components/EventPreviewCard/index.jsx
+++ src/components/EventPreviewCard/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Responsive, Transition, Segment, Header, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import style from './style';
 import {
   openEventPreview,
@@ -9,7 +10,7 @@ import {
 } from './actions';
 import calcAge from '../../utils/time';
 import getEventColor from '../../utils/eventcolors';
-import { Link } from 'react-router-dom';
+
 
 const EventPreviewCard = (props) => {
   if (!props.eventPreview.event) {
@@ -18,8 +19,8 @@ const EventPreviewCard = (props) => {
   return (
     <div>
       <Responsive minWidth={900}>
-        <Transition visible={props.eventPreview.isOpen} animation="fade up" duration={400}>        
-          <Segment style={style.widescreen} color={getEventColor(props.eventPreview.event.category)}>
+        <Transition style={style.widescreen} visible={props.eventPreview.isOpen} animation="fade up" duration={1000}>        
+          <Segment color={getEventColor(props.eventPreview.event.category)}>
             <div>
               <Header as="h3" floated="left">{props.eventPreview.event.category.toLocaleUpperCase()}</Header>
               <Header as="p" floated="right" onClick={() => props.closeEventPreview()}>
@@ -29,25 +30,29 @@ const EventPreviewCard = (props) => {
             <br />
             <Header as="h4">{props.eventPreview.event.title}</Header>
             <p>{calcAge(props.eventPreview.event.datetime)}</p>
-            <Link to={`/view/${props.eventPreview.event.key}`}>View Incident</Link>
+            <Link to={`/view/${props.eventPreview.event.key}`} onClick={() => props.closeEventPreview()}>View Incident</Link>
           </Segment>
         </Transition>
       </Responsive>
       <Responsive maxWidth={900}>
-        <Transition visible={props.eventPreview.isOpen} animation="fly up" duration={1000}>
-          <Segment style={style.mobile} color={getEventColor(props.eventPreview.event.category)}>
-            <div>
-              <Header as="h3" floated="left">{props.eventPreview.event.category.toLocaleUpperCase()}</Header>
-              <Header as="p" floated="right" onClick={() => props.closeEventPreview()}>
-                <Icon name="close" size="mini" fitted />
-              </Header>
-            </div>
-            <br />
-            <Header as="h4">{props.eventPreview.event.title}</Header>
-            <p>{calcAge(props.eventPreview.event.datetime)}</p>
-            <Link to={`/view/${props.eventPreview.event.key}`}>View Incident</Link>
-          </Segment>
-        </Transition>
+        
+        <div style={style.mobile}>
+          <Transition style={style.mobile} visible={props.eventPreview.isOpen} animation="fly up" duration={1000}>
+            <Segment color={getEventColor(props.eventPreview.event.category)}>
+              <div>
+                <Header as="h3" floated="left">{props.eventPreview.event.category.toLocaleUpperCase()}</Header>
+                <Header as="p" floated="right" onClick={() => props.closeEventPreview()}>
+                  <Icon name="close" size="mini" fitted />
+                </Header>
+              </div>
+              <br />
+              <Header as="h4">{props.eventPreview.event.title}</Header>
+              <p>{calcAge(props.eventPreview.event.datetime)}</p>
+              <Link to={`/view/${props.eventPreview.event.key}`} onClick={() => props.closeEventPreview()}>View Incident</Link>
+            </Segment>
+          </Transition>
+        </div>
+        
       </Responsive>
     </div>
 
