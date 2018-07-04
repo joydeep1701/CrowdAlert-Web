9765271967a2642a7b392780425ce7db26f39b6b
Joydeep Mukherjee <joydeep1701@gmail.com>
2018-07-04 16:30:21 +0530

    Increased padding for event preview

--- src/containers/Viewevent/style.js
+++ src/containers/Viewevent/style.js
@@ -10,7 +10,7 @@ const styleSheet = {
     itemContainer: {
       margin: '10px',
       paddingTop: '30vh',
-      paddingBottom: '8vh',
+      paddingBottom: '26vh',
       width: '100%',
     },
     cardContainer: {
