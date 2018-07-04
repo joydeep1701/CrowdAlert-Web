fbe610b2bda16c0cc758e9a04cbd51d3dd174457
Joydeep Mukherjee <joydeep1701@gmail.com>
2018-07-04 16:32:27 +0530

    Updated routes to use protected auth

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Dimmer, Loader } from 'semantic-ui-react';
+import { checkUserAuthenticationStatus } from '../Auth/actions';
+
 // import propTypes from 'prop-types';
 import {
   Menu,
@@ -9,7 +14,22 @@ import {
 import Viewevent from '../Viewevent';
 import Feed from '../Feed';
 import CreateEvent from '../CreateEvent';
+import LoginPage from '../Auth/LoginPage';
+import SignUpPage from '../Auth/SignUpPage';
+import ConfirmEmail from '../Auth/confirmEmail';
 
+const PrivateRoute = ({ component: Cmp, auth: Auth, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) => {
+      if (Auth) {
+        return (<Cmp {...props} />);
+      }
+      return (<Redirect to="/login/" />);
+    }
+  }
+  />
+);
 /**
  * [App Main entry point of the App]
  * @extends Component
@@ -19,21 +39,48 @@ class App extends Component {
     super(props);
     this.state = {};
   }
+  componentWillMount() {
+    if (window.localStorage.getItem('shouldBeLoggedIn')) {
+      this.props.checkUserAuthenticationStatus();
+    }
+  }
   render() {
     // console.log(this.state);
     return (
-      <Sidebar>
-        <div>
-          <Menu />
-        </div>
-        <Route exact path="/view/:eventid" component={Viewevent} />
-        <Route exact path="/create" component={CreateEvent} />
-        <Route exact path="/" component={Feed} />
-        
+      <div>
+        {this.props.authenticating ?
+          <Dimmer active inverted>
+            <Loader>Authenticating</Loader>
+          </Dimmer>
+        : null }
+        <Sidebar>
+          <div>
+            <Menu />
+          </div>
+          <Route exact path="/view/:eventid" component={Viewevent} />
+          <PrivateRoute exact path="/create" component={CreateEvent} auth={this.props.isLoggedIn} />
+          <Route exact path="/" component={Feed} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/signup" component={SignUpPage} />
+          <Route exact path="/auth/confirmEmail" component={ConfirmEmail} />
 
-      </Sidebar>
+        </Sidebar>
+        <BottomBar />
+      </div>
+
     );
   }
 }
-
-export default App;
+const mapStateToProps = (state) => {
+  const { isLoggedIn, authenticating } = state.auth;
+  return {
+    isLoggedIn,
+    authenticating,
+  };
+};
+const mapDispatchToProps = dispatch => (
+  bindActionCreators({
+    checkUserAuthenticationStatus,
+  }, dispatch)
+);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
