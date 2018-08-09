c582a979c49057f0f372195e65200e3de3525028
Joydeep Mukherjee <joydeep1701@gmail.com>
2018-08-10 02:00:34 +0530

    Updated build number

--- src/index.js
+++ src/index.js
@@ -82,7 +82,7 @@ registerServiceWorker();
       document.body.removeChild(dimmer);
       document.body.setAttribute('class', '');
       window.removeEventListener('load', removeDimmer);
-      alert("Production Build: 09-Aug");
+      alert("Production Build: 10-Aug");
     }, delay);
   }
   
